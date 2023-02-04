6633d55b11564cddc0f16990b9912d34647d019c
wasuken <wevorence@gmail.com>
2023-02-04 16:26:24 +0900

    upd

--- setup_today.sh
+++ setup_today.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-function create(){
-	fpath=$1
-	dir=`dirname $fpath`
-	if [ ! -d $dir ]; then
-		mkdir -p $dir
-	fi
-	if [ ! -f $dir ]; then
-		touch $fpath
-	fi
+# 日誌作成
+function diary(){
+    # diary dir
+    pathdt=`date '+%Y/%m/'`
+    dir="./diary/${pathdt}"
+    if [ ! -d $dir ]; then
+		    mkdir -p $dir
+    fi
+    namedt=`date '+%d'`
+    path="${dir}/${namedt}.md"
+    touch $path
+}
+# 進捗日報ディレクトリ作成
+function repo(){
+    # drepo dir
+    pathdt=`date '+%Y/%m/%d/'`
+    dir="./drepo/${pathdt}"
+
+    if [ ! -d $dir ]; then
+		    mkdir -p $dir
+    fi
+}
+# 進捗logファイル作成
+function log(){
+    pathdt=`date '+%Y/%m/%d'`
+    path="./drepo/${pathdt}/log.md"
+    touch $path
 }
 
-
-dt=`date '+%Y/%m/%d'`
-# diary
-fpath="./diary/${dt}.md"
-create $fpath
-
-# drepo
-fpath="./drepo/${dt}/log.md"
-create $fpath
+while getopts drl OPT
+do
+    case $OPT in
+        d) diary ;;
+        r) repo ;;
+        l) log ;;
+        *) echo "該当なし（OPT=$OPT）";;
+    esac
+done
